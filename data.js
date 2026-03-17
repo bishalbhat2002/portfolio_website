63089dc48f0cea34b381c4c71769f1fd9fddf1e5
Bishal Bhat <bishalbhat2002@gmail.com>
2026-03-17 22:34:16 +0545

    add project section

---
+++ data.js
@@ -0,0 +1,41 @@
+// Tools and Technologies Here...
+export const toolsData = ["HTML", "CSS", "JavaScript", "React.js", "Tailwind CSS", "Daisy UI", "Shadcn/UI", "Express.js", "Node.js", "MongoDB", "MySQL", "PostgreSQL", "Socket.io", "Redux", "Core PHP"];
+
+
+// Data for projects Here...
+export const projectsData = [
+     {
+          name: "FWU Sathi - A social App",
+          description: "FWU Sathi is a social platform for CSIT students to register, log in, like, comment, and connect with peers. It supports student search with filters, real-time group messaging, notifications, centralized notes access, profile management, and strong input validation.",
+          tags: ["react.js", "tailwind CSS", "express.js", "node.js", "mongoDB", "socket.io", "redux"],
+          thumbnail: "/projects/fwu-sathi/home-page.png",
+          links:{
+               yt:"https://youtu.be/ZHAWWw8NYEw?si=Poi3Mq-VH9zZtGwD",
+               website:"",
+               code:"https://github.com/bishalbhat2002/FWU-Sathi"
+          }
+     },
+     {
+          name: "Real Time Chat App",
+          description: "A real-time chat application with a clean, responsive interface that allows users to register, log in, and instantly communicate with others. It provides smooth messaging experience with simple navigation and user-friendly design for seamless interaction.",
+          tags: ["react.js", "tailwind CSS", "daisy UI", "express.js", "node.js", "mongoDB", "socket.io", "redux"],
+          thumbnail: "/projects/chat-app/chat-app.png",
+          links:{
+               yt:"https://youtu.be/WgkMqghYcdA?si=NgWBcREvViprXfhN",
+               website:"",
+               code:"https://github.com/bishalbhat2002/Real-Time-Chat-APP__MERN"
+          }
+     },
+{
+          name: "Student Management System",
+          description: "A Student Management System with a clean, user-friendly interface that supports Admin, Teacher, and Student roles. Admin manages records, fees, results, and schedules; teachers handle attendance and notes; students access courses, results, and attendance.",
+          tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+          thumbnail: "/projects/sms/admin.png",
+          links:{
+               yt:"https://youtu.be/k1pUgNJp3Is?si=HhdUPlRHTsch2B5O",
+               website:"",
+               code:"https://github.com/bishalbhat2002/Student-Management-System"
+          }
+     },
+
+]
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import { projectsData } from "./data.js";
+import { toolsData } from "./data.js";
+
+
 window.addEventListener("DOMContentLoaded", ()=>{
      const navLinks = document.querySelectorAll(".navlink");
      navLinks[0].classList.add("active-navlink");
@@ -42,5 +46,52 @@ document.addEventListener("mouseleave", (e)=>{
 
 
 
+// console.log(projectsData)
 
-// console.log("FEfwe")
+// Dymanically adding projects to the projects section
+const projectsContainer = document.getElementById("project-showcase-container");
+
+projectsData.forEach(project=>(
+     projectsContainer.innerHTML += `<div class="project-card">
+                            <div class="project-image-container">
+                                <img src="${project.thumbnail}" alt="Project Image">
+                            </div>
+                            <div class="project-description">
+                                <h3>${project?.name}</h3>
+                                <p>${project?.description}</p>
+                            </div>
+                            <div class="project-tags-container">
+                              ${
+                                   project.tags?.map(tag=>(
+                                        `<span class="project-tag">${tag}</span>`
+                                   ))
+                              }
+                            </div>
+                            <div class="project-options">
+                              ${project?.links?.yt ? `<a href="${project.links.yt}" target="_blank">Demo video</a>` : ""}
+                              ${project?.links?.website ? `<a href="${project.links.website}" target="_blank">Website</a>` : ""}
+                              ${project?.links?.code ? `<a href="${project.links.code}" target="_blank">Code</a>` : ""}
+                            </div>
+                        </div>`
+))
+
+
+// Dynamically adding tools to the tools section
+const sliderContainer = document.getElementById("slider-container");
+
+const addSliderItems = (tools)=>(
+          sliderContainer.innerHTML += `<ul class="slider">
+                         ${tools.map(tool=>(
+                              `<li>${tool}</li>`
+                         ))} 
+                         ${tools.map(tool=>(
+                              `<li>${tool}</li>`
+                         ))}
+                         ${tools.map(tool=>(
+                              `<li>${tool}</li>`
+                         ))}
+                         </ul>`
+     )
+
+     addSliderItems(toolsData);
+     addSliderItems(toolsData);
